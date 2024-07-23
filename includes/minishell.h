bed0d3e32d86094ae930281c91183bc3a6451827
Yoann Lenoel <ylenoel@bess-f2r5s13.clusters.42paris.fr>
2024-07-23 14:33:36 +0200

    minishell: THE BEGINNING

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ylenoel <ylenoel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:31:06 by ylenoel           #+#    #+#             */
-/*   Updated: 2024/07/22 16:32:13 by ylenoel          ###   ########.fr       */
+/*   Updated: 2024/07/22 16:56:12 by ylenoel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,42 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
-										/* EXEC */
+typedef enum
+{
+	STDIN_REDIR = 0,
+	HERE_DOC = 1,
+	STDOUT_REDIR = 2,
+	STDOUT_APPEND = 3,	
+}	e_redir;
+
+typedef struct s_data // Structure Globale
+{
+	/* Pour l'exec */
+	int	**pipefds;
+	int	**pids;
+	
+	t_heredoc;
+	t_list;
+	int	errno; // à remplir avec waitpid et avec signal
+}	t_data;
+
+typedef struct s_heredoc // Liste chaînée
+{
+	char *limiter; // EOF du here_doc
+	int node; // Index de la node (Important pour savoir sur quel here_doc on se situe si il y en a plusieurs)
+	t_heredoc *next;
+}	t_heredoc;
+
+typedef struct s_list
+{
+	char *full_string; // Str complète avec les redirections
+	char **cmd; // cmd avec options et arguments mais sans redirections
+	e_redir redirect;
+	t_list *next;
+}	t_list;
+
+/* PARSING */
+/* EXEC */
 
 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,13 @@
 /*   By: ylenoel <ylenoel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:33:40 by ylenoel           #+#    #+#             */
-/*   Updated: 2024/07/22 16:33:41 by ylenoel          ###   ########.fr       */
+/*   Updated: 2024/07/22 17:32:16 by ylenoel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+int main()
+{
+	
+}
