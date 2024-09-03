7d8bbbdd1adc508313c347f090b8ae2e54e188f8
Amaury Berion <aberion@paul-f4Br4s5.clusters.42paris.fr>
2024-09-03 13:20:20 +0200

    oui

--- src/builtin_env_ex.c
+++ src/builtin_env_ex.c
@@ -6,22 +6,23 @@
 /*   By: aberion <aberion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 16:00:00 by aberion           #+#    #+#             */
-/*   Updated: 2024/08/30 17:20:42 by aberion          ###   ########.fr       */
+/*   Updated: 2024/09/03 12:53:01 by aberion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <stdlib.h>
-
+#include <limits.h>
+#include <inttypes.h>
 
 long long	c_toll(const char *str)
 {
 	int				minus;
-	size_t	result;
+	u_int64_t result;
 	int				i;
 
 	i = 0;
-	result = 0;           
+	result = 0;
 	minus = 1;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
@@ -35,7 +36,7 @@ long long	c_toll(const char *str)
 	if(str[i] == '+' || str[i] == '-')
 	{
 		ft_putstr_fd("exit: numeric argument required\n", 2);
-		return 0;
+		return 2;
 	}
 	int j = 0;
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
@@ -45,7 +46,13 @@ long long	c_toll(const char *str)
 		i++;
 		j++;
 	}
-	return (result *= minus);
+	if ((j > 19) || (minus == 1 && result > INT64_MAX) 
+	|| (minus == -1 && result > (u_int64_t)INT64_MAX + 1))
+	{
+		ft_putstr_fd("exit: numeric argument required\n", 2);
+		return 2;
+	}
+	return ((long long)(result *= minus));
 }
 
 void builtin_env(t_data *s_data)
@@ -64,15 +71,30 @@ int check_args_ex(char *cmd)
 	
 	splitted = ft_split(cmd, ' ');
 	if (splitted[2])
-		return -1;
-	
+		return 1;
+	if (splitted[1])
+		return 42;
 	return 0;
 }
 
 void builtin_exit(t_data *s_data, char *cmd)
 {
-    (void)cmd;
+	long long dehors;
+
+	
+	if (check_args_ex(cmd) == 1)
+	{
+    	free_t_data(s_data);
+		exit(1);
+	}
+	if (check_args_ex(cmd) == 42)
+	{
+		dehors = c_toll(cmd);
+		free_t_data(s_data);
+		exit(dehors);
+	}
+	dehors = s_data->exit_status;
     free_t_data(s_data);
 	// ft_putstr_fd("exit", 2);
-    exit(s_data->exit_status);
+    exit(dehors);
 }
